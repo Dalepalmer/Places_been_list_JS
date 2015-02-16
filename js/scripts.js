41ab8510892514f01e4e2a2394e9192c6188ecbc
Frank <student@epicodus.com>
2015-02-16 12:14:38 -0800

    add more place details

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,25 @@ $(document).ready(function() {
     event.preventDefault();
 
     var newInputPlace = $("input#new-place").val();
+    var newPlaceCountry = $("input#new-place-country").val();
+    var newPlaceSeason = $("input#new-place-season").val();
+    var newPlaceParticulars = $("input#new-place-notes").val();
+    var newPlace = {placeName: newInputPlace, placeCountry: newPlaceCountry, placeSeason: newPlaceSeason, placeNotes: newPlaceParticulars };
 
-    var newPlace = {placeName: newInputPlace };
 
     $("ul#visited-places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
 
-    $("input#new-place").val("");
+    // $("input#new-place").val("");
+    // $("input#new-place-country").val("");
+    // $("input#new-place-season").val("");
 
-    $()
-
+    $(".place").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeName + "");
+      $(".placeName").text(newPlace.placeName);
+      $(".placeCountry").text(newPlace.placeCountry);
+      $(".placeSeason").text(newPlace.placeSeason);
+      $(".placeNotes").text(newPlace.placeNotes);
+    });
   });
 });
